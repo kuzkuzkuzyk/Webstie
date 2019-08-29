14d6e755430a28a65113707a84b3caded9f2da0c
Isabella.Kuzyk <isabella.kuzyk28@gmail.com>
2019-08-29 11:11:54 -0400

    added messages to site

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function handleClick(){
   database.push(input.value);
   input.value = '';
 }
+
+
+
+database.on('child_added', function(data){
+const messageDiv = document.querySelector('.messages')
+const message = document.createElement('p')
+message.innerText = data.val();
+messageDiv.appendChild(message);
+});
